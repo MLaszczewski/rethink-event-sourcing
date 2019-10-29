d139a017e52a401b4d18474414105db5cb6e2e70
Michał Łaszczewski <michal@emikse.com>
2019-10-29 14:29:48 +0100

    rethinkdb reconnect

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const r = require.main.rethinkdb || require('rethinkdb')
+const r = require('rethinkdb-reconnect')
 const Service = require("./lib/Service.js")
 
 module.exports = (config) => {
@@ -17,30 +17,39 @@ module.exports.command = async function(connection, service, command, parameters
   }
   cmd.state = "new"
   cmd.timestamp = new Date()
+
   let svc = service.definition ? service.definition.name : service.name || service
-  let result = await r.table( svc + "_commands" )
-      .insert(cmd, { conflict: (id,o,n) => n.merge(o) }).run(connection)
+  let result = await connection.run(
+      r.table( svc + "_commands" ).insert(cmd, { conflict: (id,o,n) => n.merge(o) })
+  )
   let commandId = cmd.id || result.generated_keys[0]
-  let changesStream = await r.table( svc + '_commands' ).get(commandId)
-      .changes({ includeInitial: true  }).run(connection)
+
   return new Promise( (resolve, reject) => {
-    changesStream.each( (err, result) => {
-      if(err) {
-        changesStream.close();
-        reject(err)
-        return false
-      }
-      let val = result.new_val
-      if(val.state == "done") {
-        resolve(val.result)
-        changesStream.close()
-        return false
-      }
-      if(val.state == "failed") {
-        reject(val.error)
-        changesStream.close()
-        return false
-      }
-    })
+    const readResult = async () => {
+      let changesStream = await connection.run(
+          r.table( svc + '_commands' ).get(commandId).changes({ includeInitial: true  })
+      )
+      changesStream.each( (err, result) => {
+        if(err) {
+          changesStream.close()
+          if(connection.handleDisconnectError(err)) return readResult()
+          reject(err)
+          return false
+        }
+        let val = result.new_val
+        if(val.state == "done") {
+          resolve(val.result)
+          changesStream.close()
+          return false
+        }
+        if(val.state == "failed") {
+          reject(val.error)
+          changesStream.close()
+          return false
+        }
+      })
+    }
+    readResult()
   })
+
 }
